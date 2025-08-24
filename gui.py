60811e6d2a4d62e1d6f697067cbffb46f2c9f1a7
snehabiswas8806 <snehab8806@gmail.com>
2025-08-24 20:50:26 +0530

    draft

--- gui.py
+++ gui.py
@@ -1,10 +1,15 @@
 
 import streamlit as st
+<<<<<<< Updated upstream
+=======
+import spreadsheet
+import locationcontroller
+>>>>>>> Stashed changes
 
 #--HEADER SECTION--
 with st.container():
-    st.set_page_config(page_title="Safe Water Locator", layout="wide")
-    st.subheader("Safe Water Locator")
+    st.set_page_config(page_title="Can You Drink It?", layout="wide")
+    st.subheader("Can You Drink It?")
     st.title("-Built by TheClique ")
     st.write("This website uses location services to provide users with information on safe and clean water sources nearby. Developed to support Sustainable Development Goal 6 (SDG 6), it aims to promote access to safe water and improve water management for healthier communities.")
     st.write("[Learn more about SDG6>](https://sdgs.un.org/goals/goal6)")
@@ -16,8 +21,13 @@ with st.container():
     col1, col2, col3 = st.columns(3)
     with col2:
         if st.button("Use Current Location \U0001F4CD"):
+<<<<<<< Updated upstream
 
 
+=======
+            arr = locationcontroller.getLocation()
+            spreadsheet.findMatch(arr[0],arr[1],arr[2])
+>>>>>>> Stashed changes
         st.subheader("Enter Location Details: ")
         state_name=st.text_input("Enter State: ")
         district_name=st.text_input("Enter District: ")
@@ -25,6 +35,7 @@ with st.container():
         withincol1, withincol2, withincol3 = st.columns(3)
         with withincol2:
             if st.button("Check"):
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -48,4 +59,7 @@ with st.container():
 >>>>>>> Stashed changes
 =======
     st.write("---")                
+>>>>>>> Stashed changes
+=======
+                spreadsheet.findMatch(state_name,district_name,county_name_name)
 >>>>>>> Stashed changes
