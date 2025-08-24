f444b8fa98cdc1d94f19faf5e4744f6973197db6
Rajashmita Seth <149235237+rajashmitaseth@users.noreply.github.com>
2025-08-24 16:42:30 +0530

    file

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import psycopg2
 import pandas as pd
-
+import csv
+import openpyxl
 
 connection = psycopg2.connect(
     host = "localhost",
@@ -14,12 +15,52 @@ cursor = connection.cursor()
 
 #statements
 # cursor.execute("CREATE DATABASE waterdata;")
-cursor.execute("CREATE TABLE IF NOT EXISTS water_quality (id SERIAL PRIMARY KEY, place VARCHAR(100), ph FLOAT, tds FLOAT, hardness FLOAT, nitrate FLOAT);")
-
-connection.commit()
+cursor.execute("CREATE TABLE IF NOT EXISTS water_quality (id SERIAL PRIMARY KEY, place VARCHAR(100), ph VARCHAR(100), tds VARCHAR(100), hardness VARCHAR(100), nitrate VARCHAR(100));")
 
 table = pd.read_sql("SELECT * FROM water_quality;", connection)
 print(table)
+
+import requests
+
+# url = "https://aikosha-api.indiaai.gov.in/akp/idp/api/v1/dataset-public/download-dataset?datasetIdentifier=jjm_water_source_water_quality_data&versionNumber=1'"
+# response = requests.get(url)
+
+# if response.status_code != 200:
+#     print("Failed to fetch data.")
+#     exit(1)
+
+# data = response.json()
+
+# for row in data:
+#     print(row)
+#     for a in row:
+#         print(a)
+    # cursor.execute("INSERT INTO water_quality (place,ph,tds,hardness,nitrate) VALUES (%s,%s,%s,%s,%s)", (
+    #     row.get("DistrictName"),
+    #     row.get("PH tested value (NA)"),
+    #     row.get("TDS tested value (mg/l)"),
+    #     row.get("Nitrate tested value (mg/l)")
+    # ))
+
+with open('book2.csv', mode='r', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(row)
+        #print(', '.join(row))
+        
+#not working
+# path = "book1.xlsx"
+# wb = openpyxl.load_workbook(path)
+# sheet = wb.active
+# cells = sheet['A1':'X16976']
+# for i in range(1, 16976):
+#     cell = sheet.cell(row=i, column=j)
+    
+# for j in range(1,24):
+#     cell = sheet.cell(row=i, column=j)
+#     print(cell)
+
+connection.commit()
 
 cursor.close()
 connection.close()
